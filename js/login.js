dedb2fc7212842a104bc854f3593bb0910240613
AyshaUrmi <ayshaismail021@gmail.com>
2023-07-22 13:59:42 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.getElementById("button submit").addEventListener("click",function(){
+
+
+    const emailField=document.getElementById('emailid');
+    const email= emailField.value;
+    console.log(email);
+
+});
+
